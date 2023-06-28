5bb1d60f261dd1d55cfd80ec54c52ed4c629063c
Joshua DeiAlorse <joshuadeialorse@gmail.com>
2023-06-28 05:59:01 +0000

    0x10. Python - Network #0

---
+++ 0x10-python-network_0/100-status_code.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Sends a request to a URL passed as an argument, 
+# and displays only the status code
+
+curl -s -o /dev/null -w "%{http_code}" "$1"
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module that finds the hightest number in an array.
+"""
+
+def find_peak(list_of_integers):
+
+    """
+    Find hightest number in array
+    """
+
+    if bool(list_of_integers) is False:
+        return None
+    list_of_integers.sort()
+
+    return list_of_integers[-1]
