c810c1b4adacfb3ba8a904b7333e31607dc6c485
YoungKing-Joshua <joshuadeialorse@gmail.com>
2023-06-27 23:00:54 -0700

    s

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script that makes a request to the body of the response.
 
-curl -s -L -X PUT -d user_id=98 -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -d user_id=98 -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
