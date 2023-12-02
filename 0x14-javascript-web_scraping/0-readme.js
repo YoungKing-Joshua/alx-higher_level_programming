235b98911c5f6fa388954e242f780c19e160f7e0
Joshua Dei-Alorse <110766878+YoungKing-Joshua@users.noreply.github.com>
2023-12-02 12:18:21 +0000

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/0-readme.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const fs = require('fs');
-
-const path = process.argv[2];
-
-fs.readFile(path, 'utf8', function (err, content) {
-  console.log(err || content);
-});
--- 0x14-javascript-web_scraping/5-request_store.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-const request = require('request');
-
-const pathU = process.argv[2];
-const pathF = process.argv[3];
-
-request(pathU).pipe(fs.createWriteStream(pathF));
